33ce34e0812387bd23f51f3e8d17f5a11ef41413
Parth R. Doshi <122556195+doshi36@users.noreply.github.com>
2025-04-09 17:55:22 -0400

    Update gps_enable.py

--- GPS/gps_enable.py
+++ GPS/gps_enable.py
@@ -1,6 +1,10 @@
-from machine import UART
+from machine import UART, Pin
 from micropyGPS import MicropyGPS
 import time
+
+# # Intialize GPS EN pin as GPIO26 on ESP32
+gps_en = Pin(26, Pin.OUT)
+gps_en.value(1)
 
 # Initialize GPS Parser Module
 gps = MicropyGPS()
@@ -8,20 +12,28 @@ gps = MicropyGPS()
 # UART2 on GPIO7 (RX) and GPIO8 (TX)
 uart = UART(2, baudrate=9600, tx=8, rx=7, timeout=10)
 
-print("Reading GPS data...")
+def read_gps_data():
+    if gps_en.value():
+        if uart.any():
+            line = uart.readline()
+            if line:
+                try:
+                    line = line.decode('utf-8').strip()
+                    for char in line:
+                        sentence_type = gps.update(char)
+                    if sentence_type:
+                        formatted_coordinates = (gps.latitude, gps.longitude)
+                        speed = gps.speed_string('kph')
+                        print(formatted_coordinates, speed)
+                except:
+                    pass
+    else:
+        print("GPS is disabled (EN pin LOW)")
 
-while True:
-    if uart.any():
-        line = uart.readline()
-        if line:
-            try:
-                line = line.decode('utf-8').strip()
-                for char in line:
-                    sentence_type = gps.update(char)
-                if sentence_type:
-                    formatted_coordinates = (gps.latitude, gps.longitude)
-                    speed = gps.speed_string('kph')
-                    print(formatted_coordinates, speed)
-            except:
-                pass
-    time.sleep(1)
+def main():
+    print("Starting GPS Logger...")
+    while True:
+        read_gps_data()
+        time.sleep(1)
+
+main()
